2592e1700ee26c00ab8633c400f9a41ae9463679
Nikoloz Merlani <nikusha.merlani@gmail.com>
2024-07-18 15:56:57 +0400

    game engine added on branch2

--- cargame.py
+++ cargame.py
@@ -1,1 +1,28 @@
+while True:
+    command = input("> ")
+    command = command.lower()
+    if command == "start":
+        if started == False:
+            print("lesgo")
+            started = True
+        else:
+            print("already started")
+    elif command == "stop":
+        if started == True:
+            print("stopped")
+            started = False
+        else:
+            print("already stopped")
+    elif command == "help":
+        print("start - to start the car")
+        print("stop - to stop the car")
+        print("help - helpdesk")
+        print("quit - quit the game")
 
+
+    elif command == "quit":
+        print("bye")
+        break
+    else:
+        print("weird input")
+    
